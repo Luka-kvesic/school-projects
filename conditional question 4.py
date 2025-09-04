109e9c804ab16c3cd4a22a6d0d1a40c309d277e2
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-04 21:06:38 +0100

    Add files via upload

---
+++ conditional question 4.py
@@ -0,0 +1,8 @@
+#author: Luka kvesic
+#date: 04 september 2025
+#description: what their favoutite color is and your opinion
+color = input('what is your favourite color: ')
+if (color == 'red')  or (color == 'Red') or (color == 'RED'):
+    print('red is also my favourite color')
+else:
+    print('i dont like',color,'i prefer red' )
