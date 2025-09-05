b38f8d591c3fa6032e40d2509667740672ae1fd8
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-05 09:34:23 +0100

    Add files via upload

---
+++ conditional question 5 (fixed version).py
@@ -0,0 +1,13 @@
+#author: Luka kvesic
+#date: 04 september 2025
+#description: ask if its raining and windy and give the appropriate response
+raining = input('is it raining: ')
+if (raining == 'yes') or (raining == 'Yes') or (raining == 'YES'):
+    windy = input('is it windy: ')
+    if (windy == 'yes') or (windy == 'Yes') or (windy == 'YES'):
+        print('its too windy for an umbrella')
+    else:
+        print('bring an umbrella')
+else:
+    print('have a good day')
+        
