577965d8c12415ff754718ead7c8f61c3f033682
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-06 16:56:09 +0100

    Update conditional question 9.py
    finished

--- conditional question 9.py
+++ conditional question 9.py
@@ -4,8 +4,11 @@
 units = float(input('how many units did you use: '))
 
 if (units <=100):
-    calculation = units * 0.05
+    calculation = units * 0.05 
+    print('it will cost ',calculation)
 elif (units >= 100) and (units <= 250):
     calculation = units * 0.1
+    print('it will cost ',calculation)
 elif (units > 250):
-    calculation = units * 20
+    calculation = units * 0.20
+    print('it will cost ',calculation)
