068a47f4934489d1b9524824201efcef1bd9cc23
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-08 18:46:01 +0100

    Update forLoops-Q5.py

--- forLoops-Q5.py
+++ forLoops-Q5.py
@@ -1,12 +1,15 @@
 #name: luka kvesic
 #date: 09 september 2025
-#description: placeholder
+#description: ask question 5 times using for loop and add total
 
 total = 0
 
 for Q1 in range(5):
     number = int(input('enter a number: '))
     Q = input('do you want to add that number: ')
-    if (Q =='')
-    
+    if (Q == 'yes') or (Q == 'Yes') or (Q == 'YES'):
+        total = total + number
+    else:
+        pass
+print(total)
     
