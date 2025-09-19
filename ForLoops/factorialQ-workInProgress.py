7f75875427f5b1a66ee4bcbd4a3cbd0f88920785
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-19 09:45:20 +0100

    Add files via upload

---
+++ ForLoops/factorialQ-workInProgress.py
@@ -0,0 +1,12 @@
+#author: Luka kvesic
+#date: 19 september 2025
+#description: print the factorial of a non negative integer
+
+number = int(input('what is the number'))
+number1 = 0
+for i in range(number):
+    
+    number1 = number - 1
+    factorial = number * number1
+print(factorial)
+    
