66ffac23c9edf966e26f4c18ef5503b82588368d
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-19 09:31:50 +0100

    Add files via upload

---
+++ flowchartQ1.py
@@ -0,0 +1,13 @@
+#author: Luka kvesic
+#date: 19 september 2025
+#description: get area + perimter of rectangle
+
+
+length = int(input('what is the length: '))
+width = int(input('what is the width: '))
+
+area = width * length
+perimeter = width*2 + length*2
+
+print(area)
+print(perimeter)
