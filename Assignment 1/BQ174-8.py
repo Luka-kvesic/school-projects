d5b92138250506b05623f09847e499fd60c88392
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-26 16:48:48 +0100

    Update BQ174-8.py

--- Assignment 1/BQ174-8.py
+++ Assignment 1/BQ174-8.py
@@ -3,8 +3,9 @@
 #description: convert cm to inches and feet
 cm = int(input('give your height in cm: '))
 
-inches = cm / 2.54
-feet = inches / 12
+inches = cm // 2.54
+feet = inches // 12
+remainingInches = inches % 12
 
 print('you are',inches,'inches tall')
-print('you are',feet,'feet tall')
+print('you are',feet,'feet and',remainingInches,'inches tall')
