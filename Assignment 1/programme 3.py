6a87ff4a60c8cf3a59b3a410d486371f388eae05
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-26 16:37:19 +0100

    Update programme 3.py

--- Assignment 1/programme 3.py
+++ Assignment 1/programme 3.py
@@ -3,10 +3,11 @@
 #description: ask for two numbers and add them
 number1 = input('enter the first number: ')
 number2 = input('enter the second number: ')
-number1 = int(number1)
-number2 = int(number2)
+number1 = float(number1)
+number2 = float(number2)
 
 total = number1 + number2
-print('the sume of those numbers is', total)
+print('the sum of those numbers is', total)
+
 
 
