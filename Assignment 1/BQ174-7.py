f8861e60a742b0c7424121fa759304469d2ba596
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-09-26 16:44:12 +0100

    Update BQ174-7.py

--- Assignment 1/BQ174-7.py
+++ Assignment 1/BQ174-7.py
@@ -2,12 +2,13 @@
 #date: 02 september 2025
 #description: averages 5 marks to one average grade
 
-number1 = int(input('enter the first grade: '))
-number2 = int(input('enter the second grade: '))
-number3 = int(input('enter the third grade: '))
-number4 = int(input('enter the fourth grade: '))
-number5 = int(input('enter the fifth grade: '))
+number1 = float(input('enter the first grade: '))
+number2 = float(input('enter the second grade: '))
+number3 = float(input('enter the third grade: '))
+number4 = float(input('enter the fourth grade: '))
+number5 = float(input('enter the fifth grade: '))
 
 averageGrade = (number1 + number2 + number3 + number4 + number5) / 5
 
 print('the average mark is', averageGrade)
+
