44f67efc2772b6201cb00b49a47c26e7214c1f00
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-03 15:30:23 +0100

    Update WhileLoopsQ3.py

--- Assignment 6/WhileLoopsQ3.py
+++ Assignment 6/WhileLoopsQ3.py
@@ -1,13 +1,12 @@
 #author: Luka kvesic
-#date: 2 october 2025
+#date: 3 october 2025
 #description: take integer number and output all even numbers
 
 number = int(input('give a number: '))
-even_number = 0
-while even_number < number:
-    if (even_number+1) == number:
-        break
-    even_number += 2
-    print(even_number)
+count = 0
+while count < number:
+    count += 1
+    if count%2 == 0:
+        print(count)
     
     
