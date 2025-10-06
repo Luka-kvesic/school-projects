12226bbeea37708c0c99601ce91f7652618e22fd
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-06 18:29:20 +0100

    Update codingExcersises-Q1.py

--- Assignment 7/codingExcersises-Q1.py
+++ Assignment 7/codingExcersises-Q1.py
@@ -1,23 +1,33 @@
 #author: Luka kvesic
-#date: 2 october 2025
+#date: 6 october 2025
 #description: programme that checks if a number is an armstrong number
 
 
 number = int(input('give a number: '))
 number1 = number
 digits = 0
+count = 0
+nextNumber = 0
+total = 0
+
+
 for i in range(1, number1 + 1):
     if (number < 1):
         i = number1
     else:
         number = number / 10
         digits = digits + 1
-print(digits)
+
+
 for i in range(digits, 0, -1):
-    numbers = number // ((10**i)/10000)
-    minusNumber = numbers
-    print(numbers)
-    
-    
-    
-    
+    count += 1
+    numbers = number1 // (10**(i-1))
+    nextNumber = numbers
+    if i != digits:
+        nextNumber = nextNumber - minusNumber
+    minusNumber = numbers*(10)
+    total += nextNumber**digits
+
+
+if total == number1:
+    print(total,'is an armstrong number')
