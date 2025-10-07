d27b54a7049b609d3df21ece41d3086527179f15
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-07 19:51:54 +0100

    Update A8-codingExcersises-Q1.py

--- Assignment 8/A8-codingExcersises-Q1.py
+++ Assignment 8/A8-codingExcersises-Q1.py
@@ -1,16 +1,13 @@
 #author: Luka kvesic
 #date: 7 october 2025
-#description: programme that checks if final 
+#description: programme that checks if final number is 4 or 8 and state it, if neither print neither
 
 
 number = int(input('give a number: '))
 number1 = number
 digits = 0
-count = 0
 nextNumber = 0
-total = 0
-
-
+minusNumber = 0
 for i in range(1, number1 + 1):
     if (number < 1):
         i = number1
@@ -20,20 +17,17 @@ for i in range(1, number1 + 1):
 
 
 for i in range(digits, 0, -1):
-    count += 1
     numbers = number1 // (10**(i-1))
     nextNumber = numbers
-    if i != digits:
-        nextNumber = nextNumber - minusNumber
+    nextNumber = nextNumber - minusNumber
     minusNumber = numbers*(10)
-    I = i
-    if I == 0:
-        if nextNumber == 4:
-            print('it ends with 4')
-        elif nextNumber == 8:
-            print('it ends with 8')
-        else:
-            print('it doesnt end with either')
+
+if nextNumber == 4:
+    print('it ends with 4')
+elif nextNumber == 8:
+    print('it ends with 8')
+else:
+    print('it doesnt end with either')
 
     
 
