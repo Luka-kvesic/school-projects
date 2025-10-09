94571b63c929f08410eb4a7b72fd32ca8519df00
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-09 09:46:06 +0100

    Add files via upload

---
+++ Assignment 8/A8-codingExcersises-Q6.py
@@ -0,0 +1,13 @@
+#author: Luka kvesic
+#date: 9 october 2025
+#description: programme that prints integer between 1 and N divisible by m and state if its even or odd
+number = int(input('give a number'))
+divisionNumber = int(input('give a number to be the divider'))
+
+for i in range(1, number + 1):
+        if i%divisionNumber == 0 and i%2==:
+            print('number', i,'is divisible and even' )
+        elif passedNumber%2 == 1:
+            print('number', i,'is divisible and odd' )
+    
+
