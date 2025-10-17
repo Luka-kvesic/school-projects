9b33e566fe3e5208a4a79c2ce459b8233679906b
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-17 09:01:55 +0100

    Add files via upload

---
+++ Assignment 9/Assignment9Q2.py
@@ -0,0 +1,10 @@
+#author: Luka kvesic
+#date: 9 october 2025
+#description: ask for name and display the length of it
+
+name = input("what is your name: ")
+surname = input("whats your surname: ")
+combinedName = (name +" "+ surname)
+lengthOfFullName = len(combinedName)
+print(combinedName)
+print('the length of your full name is',lengthOfFullName)
