a3c439a97345d31a6099c4af6a78cbce6a2b9339
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-17 09:21:46 +0100

    Update Assignment9Q1.py

--- Assignment 9/Assignment9Q1.py
+++ Assignment 9/Assignment9Q1.py
@@ -1,8 +1,9 @@
 #author: Luka kvesic
-#date: 9 october 2025
+#date: 17 october 2025
 #description: ask for name and display the length of it
 
 name = input("what is your name: ")
 nameLength = len(name)
 
 print('the length of your name is', nameLength)
+
