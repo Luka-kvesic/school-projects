5ef8735cef09ab37e82696d8ca973d3da55028a4
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-17 08:56:14 +0100

    Add files via upload

---
+++ Assignment9Q1.py
@@ -0,0 +1,8 @@
+#author: Luka kvesic
+#date: 9 october 2025
+#description: ask for name and display the length of it
+
+name = input("what is your name: ")
+nameLength = len(name)
+
+print('the length of your name is', nameLength)
