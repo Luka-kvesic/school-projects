639f273efede08d2d283ddc354642ed5aec6c8d3
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-21 14:28:36 +0100

    Add files via upload

---
+++ Assignment 10/Assignment10Q2i.py
@@ -0,0 +1,10 @@
+#author: Luka kvesic
+#date: 21 october 2025
+#description: find length of string using A for loop
+count = 0
+string = input('give me a string: ')
+
+for characters in string:
+    count += 1
+print(count)
+
