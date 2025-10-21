f84bf4b7b49d9060609fcf2564eec9d03e5bc1ca
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-21 21:48:04 +0100

    Add files via upload

---
+++ Assignment 10/Assignment10Q2ii.py
@@ -0,0 +1,12 @@
+#author: Luka kvesic
+#date: 21 october 2025
+#description: find length of string using A while loop
+i = 0
+string = input('give a string: ')
+
+
+while i < len(string):
+    i += 1
+print(i)
+
+    
