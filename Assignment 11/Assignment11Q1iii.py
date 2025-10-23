52ce2e7d153161b3d6c55d99db424bb1c7d054c9
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-23 09:13:43 +0100

    Update Assignment11Q1iii.py

--- Assignment 11/Assignment11Q1iii.py
+++ Assignment 11/Assignment11Q1iii.py
@@ -1,9 +1,10 @@
 #author: Luka kvesic
 #date: 21 october 2025
-#description: programme that reverses a string using a for loop
+#description: programme that reverses a string using slicing
 newWord = ''
 
 string = input('enter a string: ')
 
 newWord = string[::-1]
+
 print(newWord)
