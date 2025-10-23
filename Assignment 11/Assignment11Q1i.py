b7205236042d828d542506f6dff938a9aee14a6c
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-10-23 09:09:18 +0100

    Update Assignment11Q1i.py

--- Assignment 11/Assignment11Q1i.py
+++ Assignment 11/Assignment11Q1i.py
@@ -3,9 +3,10 @@
 #description: programme that reverses a string using a for loop
 newWord = ''
 
-string = input('enter a string')
+string = input('enter a string: ')
 
 for i in range(len(string),0,-1):
     letter = string[i-1]
     newWord += letter
 print(newWord)
+
