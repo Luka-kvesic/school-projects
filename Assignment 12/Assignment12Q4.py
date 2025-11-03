65a2227cfc2450ffc36ef37e59027c9970943b46
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-11-03 20:26:43 +0000

    Update Assignment12Q4.py

--- Assignment 12/Assignment12Q4.py
+++ Assignment 12/Assignment12Q4.py
@@ -1,4 +1,12 @@
 #author: Luka kvesic
 #date: 3 november 2025
-#description: write a programme count how many vowels a string has
+#description: checks how many vowels are in a string
+count = 0
 
+string = input('enter a string: ')
+
+for i in range(len(string)):
+    string = str.lower(string)
+    if (string[i] == 'a') or (string[i] == 'o')  or (string[i] == 'i') or (string[i] == 'u') or (string[i] == 'e') :
+        count += 1
+print('there are', count,'vowels in your string')
