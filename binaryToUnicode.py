339af7af5ece0195e562a26a6f3505b2ec83df9c
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-11-27 09:45:36 +0000

    Add files via upload

---
+++ binaryToUnicode.py
@@ -0,0 +1,103 @@
+#author: Luka kvesic
+#date: 27 nov 2025
+#description: convert unicode to binary
+
+
+fourBits = ''
+byte1 = False
+byte2 = False
+byte3 = False
+counting = False
+binary = input('give the binary value: ')
+
+
+if binary[0] == '0':
+    byte1 = True
+elif binary[0:3] == '110':
+    byte2 = True
+elif binary[0:4] == '1110':
+    byte3 = True
+
+if byte1:
+    for i in range(1, 8):
+        if (binary[i] == '0') and (counting == False):
+            counting = False
+            continue
+        elif (binary[i] == '1') and (counting == False):
+            fourBits += binary[i]
+            counting = True
+        elif (counting):
+            fourBits += binary[i]
+    if len(fourBits) < 16:
+        extraZeros = 16 - len(fourBits)
+        fourBits = fourBits[::-1]
+        for i in range(extraZeros):
+            fourBits += '0'
+        fourBits = fourBits[::-1]
+if byte2:
+    for i in range(3, 8):
+        if (binary[i] == '0') and (counting == False):
+            counting = False
+            continue
+        elif (binary[i] == '1') and (counting == False):
+            fourBits += binary[i]
+            counting = True
+        elif (counting):
+            fourBits += binary[i]
+    for i in range(10, 16):
+        if (counting):
+            fourBits += binary[i]
+
+    if len(fourBits) < 16:
+        extraZeros = 16 - len(fourBits)
+        fourBits = fourBits[::-1]
+        for i in range(extraZeros):
+            fourBits += '0'
+        fourBits = fourBits[::-1]
+
+if byte3:
+    for i in range(4, 8):
+        if (binary[i] == '0') and (counting == False):
+            counting = False
+            continue
+        elif (binary[i] == '1') and (counting == False):
+            fourBits += binary[i]
+            counting = True
+        elif (counting):
+            fourBits += binary[i]
+    for i in range(10, 16):
+        if (counting):
+            fourBits += binary[i]
+    for i in range(18, 24):
+        if (counting):
+            fourBits += binary[i]
+            
+    if len(fourBits) < 16:
+        extraZeros = 16 - len(fourBits)
+        fourBits = fourBits[::-1]
+        for i in range(extraZeros):
+            fourBits += '0'
+        fourBits = fourBits[::-1]
+        
+print(fourBits)
+
+extraStop  = 0
+extraStart = 0
+halfByte = ''
+
+for i in range(4):
+    start = 0 + extraStart
+    stop  = 4 + extraStop
+    for i in range(start, stop):
+        halfByte += fourBits[i]
+        
+    for i in range(4):
+        number += int(halfByte[i])**(3-i)
+    print(number)
+    print(halfByte)
+    halfByte = ''
+    extraStart += 4
+    extraStop  += 4
+    
+            
+            
