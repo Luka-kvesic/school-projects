2a78dcf140615db505b4674354aae4bcbbe4c97f
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-12-08 12:47:50 +0000

    Update Assignment13Q7.py

--- Assignment 13/Assignment13Q7.py
+++ Assignment 13/Assignment13Q7.py
@@ -1,9 +1,10 @@
 #author: Luka kvesic
 #date: 8 dec 2025
-#description: sum of first 50 integers that are multiple of 50
+#description: product of all even numbers of first 50 integers
 
 total = 1
 for i in range(1, 50+1):
     if i%2 == 0:
         total *= i
+
 print(total)
