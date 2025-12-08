5eca5e377d0745f56a89dbbd3fa9f56d15b5662c
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-12-08 12:47:03 +0000

    Update Assignment13Q6.py

--- Assignment 13/Assignment13Q6.py
+++ Assignment 13/Assignment13Q6.py
@@ -1,9 +1,10 @@
 #author: Luka kvesic
 #date: 8 dec 2025
-#description: sum of first 50 integers that are multiple of 50
+#description: sum of first 50 integers that are multiple of 3
 
 total = 0
 for i in range(1, 50+1):
     if i%3 == 0:
         total += i
+
 print(total)
