da440e2a15858956a97f8ddce7351dc6748ce131
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-12-08 12:38:52 +0000

    Add files via upload

---
+++ Assignment 13/Assignment13Q6.py
@@ -0,0 +1,9 @@
+#author: Luka kvesic
+#date: 8 dec 2025
+#description: sum of first 50 integers that are multiple of 50
+
+total = 0
+for i in range(1, 50+1):
+    if i%3 == 0:
+        total += i
+print(total)
