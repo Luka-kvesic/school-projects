b8a9bc5929ad2b41d1a6a11b164711176ea77f43
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2025-12-16 14:15:07 +0000

    Update NovTestRevisionP2.py

--- November assessment coding revision - part B/NovTestRevisionP2.py
+++ November assessment coding revision - part B/NovTestRevisionP2.py
@@ -1,32 +1,40 @@
 #author: Luka kvesic
-#date: 15 dec 2025
+#date: 16 dec 2025
 #description: program 2
 
 reversedWinCardNum = ''
 winCard = 8549018035096133
-total = 0
+total   = 0
 winCardString = str(winCard)
-
+evenWinCardIndices = ''
+oddWinCardIndices  = ''
 #----------------step 1----------------#
-winCardNum = winCardString[0:len(winCardString)-1]
+winCardNum    = winCardString[0:len(winCardString)-1]
 checkingDigit = winCardString[len(winCardString)-1:len(winCardString)]
 #----------------step 2----------------#
-for i in range(-1, -1*(len(winCardNum)), -1):
+for i in range(-1, -1*(len(winCardNum)+1), -1):
     reversedWinCardNum += winCardNum[i]
 print(reversedWinCardNum)
 #----------------step 3----------------#
-evenWinCardIndices = reversedWinCardNum[::2]
+for i in range(len(reversedWinCardNum)):
+    if i%2 == 0:
+        evenWinCardIndices += reversedWinCardNum[i]
+for i in range(len(reversedWinCardNum)):
+    if i%2 == 1:
+        oddWinCardIndices += reversedWinCardNum[i]
 print(evenWinCardIndices)
-oddWinCardIndices = reversedWinCardNum[1::2]
 print(oddWinCardIndices)
-
 for i in range(len(evenWinCardIndices)):
     evenDigit = evenWinCardIndices[i]
     evenDigit = int(evenDigit) *2
     if evenDigit > 9:
         evenDigit -= 9
-        #--------step 4----------------#
     total += evenDigit
+for i in range(len(oddWinCardIndices)):
+    oddDigit = oddWinCardIndices[i]
+    oddDigit = int(oddDigit) 
+        #--------step 4----------------#
+    total += oddDigit
 total += int(checkingDigit)
 #----------------step 5----------------#
 if total%10 == 0:
