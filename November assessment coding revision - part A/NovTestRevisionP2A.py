03109c02e1ceb926b68c0a2feaae11e24dfa4f67
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-01-07 20:21:38 +0000

    Update NovTestRevisionP2A.py

--- November assessment coding revision - part A/NovTestRevisionP2A.py
+++ November assessment coding revision - part A/NovTestRevisionP2A.py
@@ -12,9 +12,11 @@ i = 0
 proggrameRun = True
 cvv = 0
 while (tries > 0) and (proggrameRun):
+               #--------------Part 1--------------------#
     cardNum = input('Welcome to CardCheck. Enter your card number: ') # 7200828282828210
     print(cardNum)
     cardCopy = cardNum[-1*(len(cardNum))]
+            #--------------Part 2 and 3--------------------#
     if cardCopy == '7':
         print('This is a zincCard')
     elif cardCopy == '8':
@@ -23,6 +25,7 @@ while (tries > 0) and (proggrameRun):
         tries -= 1
         print('invalid try again')
         continue
+               #--------------Part 4--------------------#
     if len(cardNum) == 16:
         continueWithCheck = True
     else:
@@ -30,6 +33,7 @@ while (tries > 0) and (proggrameRun):
         print('invalid try again')
         continue
     if continueWithCheck:
+            #--------------Part 5--------------------#
         date = input('Enter the card expiry date: ')
         dateCopy = int(date)
         for i in range(len(date)):
@@ -37,9 +41,11 @@ while (tries > 0) and (proggrameRun):
         twoDigits = cardNum[-16:-14]
         tenthDigit = cardNum[-7]
         cvv = (dateNum * int(twoDigits)) - int(tenthDigit)
+            #--------------Part 7--------------------#
         finalNumber = cardNum[0:4]+'-'+cardNum[4:8]+'-'+cardNum[8:12]+'-'+cardNum[12:16]
         print('CVV number:', cvv)
         print('Card number:',finalNumber, 'and it is valid')
         proggrameRun = False
+
 
 
