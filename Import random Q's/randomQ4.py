0812f66abcb7c839d567c3f15310a651abb5e6f4
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-02-09 12:38:06 +0000

    Add files via upload

---
+++ Import random Q's/randomQ4.py
@@ -0,0 +1,20 @@
+#name       Luka kvesic
+#date       2/9/2026
+#programme  random guessing game between a hundored
+
+import random
+
+randomNumber = random.randint(1,100)
+triesLeft = 7
+playing = True
+while playing:
+    userGuess = int(input("guess a number between 1 and a hundred you have 7 guesses: "))
+    if userGuess == randomNumber:
+        print("you got it")
+        playing = False
+    elif userGuess != randomNumber:
+        print("try again")
+        triesLeft -= 1
+    if triesLeft <= 0:
+        playing = False
+        print("you ran out of guesses")
