c42c26da95d9166c4cd6a41570557fe5ee479b6c
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-02-09 23:14:10 +0000

    Update randomQ3.py

--- Import random Q's/randomQ3.py
+++ Import random Q's/randomQ3.py
@@ -1,21 +1,20 @@
 #name       Luka kvesic
 #date       2/9/2026
-#programme  random guessing game between a hundored
+#programme  random guessing game heads and tails
 
-import random #WHOOPS, WILL FIX PROGRAMME SOON, BECUASE THIS IS NOT THE RIGHT ONE
+import random 
 
-randomNumber = random.randint(1,100)
-triesLeft = 7
-playing = True
-while playing:
-    userGuess = int(input("guess a number between 1 and a hundred you have 7 guesses: "))
-    if userGuess == randomNumber:
-        print("you got it")
-        playing = False
-    elif userGuess != randomNumber:
-        print("try again")
-        triesLeft -= 1
-    if triesLeft <= 0:
-        playing = False
-        print("you ran out of guesses")
+userGuess = input("guess if my coin lands heads or tails, spell your answer the same way i spelt mine or im not playing: ")
+
+options = ["heads", "tails"]
+
+reality = random.choice(options)
+
+print("i landed", reality)
+print("you guessed",userGuess)
+
+if userGuess == reality:
+    print("you guessed correctly")
+else:
+    print("you guessed incorrectly")
 
