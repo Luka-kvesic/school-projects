dd7f8a32246cc6ed86d3315ef2ed01492ee213a1
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-02-12 20:40:43 +0000

    Update coding assignment with 2DigitSquareThings.py

--- coding assignment with 2DigitSquareThings.py
+++ coding assignment with 2DigitSquareThings.py
@@ -3,11 +3,13 @@
 #programme  two digit property with sum of sqaures
 
 altDigit1 = 0
+
 altDigit2 = 0
 altDigit3 = 0           # --------> WORK IN PROGRESS  <----------
 numbersThatFit = []
 digit1 = 10
 digit2 = 10
+currentList = []
 digit3 = 10
 for i1 in range(10,99):
     digit1 = i1
@@ -24,15 +26,14 @@ for i1 in range(10,99):
             altDigit2 = altDigit2[::-1]
             altDigit3 = str(digit3)
             altDigit3 = altDigit3[::-1]
-            digit1 = float(digit1)
-            digit2 = float(digit2)
-            digit3 = float(digit3)
             altDigit1 = int(altDigit1)
             altDigit2 = int(altDigit2)
             altDigit3 = int(altDigit3)
-            print([altDigit1,altDigit2,altDigit3])
             if (digit1**2) + (digit2**2) + (digit3**2) == (altDigit1**2) + (altDigit2**2) + (altDigit3**2):
                 currentList = [digit1,digit2,digit3]
-                print(currentList)
-                numbersThatFit.extend(currentList)
+                for index in range(3):
+                    for i in range(len(numbersThatFit)):
+                        if currentList[index] != numbersThatFit[i]:
+                            numbersThatFit.append(currentList[index])
 print(numbersThatFit)
+
