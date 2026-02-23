79c26f0e0c2555d68d47fd6e7352effd919a5089
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-02-23 12:57:10 +0000

    Add files via upload

---
+++ import math/import math E2.py
@@ -0,0 +1,12 @@
+#author: Luka kvesic
+#date: 2/23/2026
+#description: example 1
+import math
+
+
+r = 7.5
+
+area = 4*math.pi*math.pow(r, 2)
+volume = 4/3*math.pi*math.pow(r, 3)
+
+print(area, volume)
