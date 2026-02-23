516479ed28b05ef520c6155f12c8ff44b39c3847
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-02-23 19:44:02 +0000

    Add files via upload

---
+++ import math E5.py
@@ -0,0 +1,36 @@
+#author: Luka kvesic
+#date: 2/23/2026
+#description: excersise 5
+import math
+
+#-------------- a --------
+
+length = 16
+deg = 75
+rad = math.radians(deg)
+height = length*math.sin(rad)
+print(height)
+
+#-------------- b --------
+
+length = 20
+deg = 0
+rad = math.radians(deg)
+height = length*math.sin(rad)
+print(height)
+
+#-------------- c --------
+
+length = 24
+deg = 45
+rad = math.radians(deg)
+height = length*math.sin(rad)
+print(height)
+
+#-------------- d --------
+
+length = 24
+deg = 80
+rad = math.radians(deg)
+height = length*math.sin(rad)
+print(height)
