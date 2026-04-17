24c403c1ca8b8097dde8f6914f14e1f0d25227fd
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-04-17 09:45:03 +0100

    Add files via upload

---
+++ 2023Question16_A.py
@@ -0,0 +1,13 @@
+# Question 16 (a)
+# Name and School:luka kvesic
+
+flight_num = input("Enter your flight number: ")
+destination = input("Enter your destination: ")
+num_ppl = int(input("Enter the number of people in the travel group: "))
+print("your flight number is",flight_num)
+print("you are travelling to",destination)
+print("There are ",num_ppl,"people in your travel group")
+print("The total cost of your flights is",chr(\u20ac),)
+
+
+
