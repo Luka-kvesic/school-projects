21b82dc0ec2f4c611f06615576ec38b466b8cd9d
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-04-20 12:59:40 +0100

    Update 2023Question16_A.py

--- 2023Question16_A.py
+++ 2023Question16_A.py
@@ -7,7 +7,13 @@ num_ppl = int(input("Enter the number of people in the travel group: "))
 print("your flight number is",flight_num)
 print("you are travelling to",destination)
 print("There are ",num_ppl,"people in your travel group")
-print("The total cost of your flights is",chr(\u20ac),)
+if flight_num == "EI121":
+    cost = 520
+elif flight_num == "EI125":
+    cost = 400
+print("The total cost of your flights is",chr(0x20ac),cost*num_ppl)
 
-
+#EI121
+#orlando
+#7
 
