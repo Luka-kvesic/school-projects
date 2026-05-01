f2464394d12771d6090fda142cc3f38dcbca3e71
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-05-01 09:02:37 +0100

    Update master yoda.py

--- master yoda.py
+++ master yoda.py
@@ -4,13 +4,19 @@ sentence = input("enter a sentence: ")
 def masterYoda(sentence):
     word = ""
     yodaSentence = ""
-    for i in range(1, len(sentence)+ 2):
+    for i in range(0, len(sentence)+1):
         print(i)
-        letter = sentence[len(sentence)-i]
-        if (letter == " ") or (i == len(sentence)+2):
-            yodaSentence += word
+        reversedWord = ""
+        letter = sentence[len(sentence)-(i+1)]
+        if (letter == " ") or (i == len(sentence)):
+            for i in range(len(word)):
+                reversedWord += word[len(word)-(i+1)]
+            reversedWord += " "
+            yodaSentence += reversedWord
             word = ""
-        word += letter
+            reversedWord = ""
+        else:
+            word += letter
     return yodaSentence
         
 newSentence = masterYoda(sentence)
