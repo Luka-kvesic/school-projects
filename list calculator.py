680e94c6fbc19f1008b67c84a0aaff0006a906f7
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-05-07 09:33:58 +0100

    Update list calculator.py

--- list calculator.py
+++ list calculator.py
@@ -31,12 +31,47 @@ def medianfinder(list1):
 median  = medianfinder(list1)
 
 def modefinder(list1):
+    previousNum = 897158714571049856194561946571465
+    amount = 0
+    previousAmount = 0
     list1.sort()
-    for i in range(list1):
+    for i in range(len(list1)):
         number = list1[i]
-        if number[i]
-        
-
+        if number == previousNum:
+            amount += 1
+        elif number != previousNum:
+            if amount > previousAmount:
+                mode = list1[i-1]
+                previousAmount = amount
+                amount = 0
+        previousNum = number
+    return mode
+      
 mode = modefinder(list1)
 
 print("median",median)
+print("mode",mode)
+
+def frequencyFinder(list1):
+    frequencyList = []
+    previousNum = 897158714571049856194561946571465
+    amount = 1
+    list1.sort()
+    for i in range(len(list1)):
+        number = list1[i]
+        if number == previousNum:
+            amount += 1
+        elif number != previousNum:
+            pair = (list1[i-1],amount)
+            frequencyList.append(pair)
+            amount = 1
+        previousNum = number
+    return frequencyList
+
+frequencyList = frequencyFinder(list1)
+for i in range(len(frequencyList)):
+    numbar = frequencyList[i][0]
+    amaunt = frequencyList[i][1]
+    print(numbar, "pops up",amaunt,"times")
+
+
