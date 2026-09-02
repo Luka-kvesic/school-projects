f529300a9d50fe170f8ea11b3d2d618dd266cd2c
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-09-02 21:13:12 +0100

    Add files via upload

---
+++ LeavingCert/Linear Sort.py
@@ -0,0 +1,21 @@
+
+def LinearSort(List, x):
+    if x.isdigit():
+        x = int(x)
+    index = 0
+    while True:
+        if List[index] == x:
+            return index
+        elif List[index] == List[-1]:
+            return -1
+        
+        index += 1
+        
+
+
+itemList = eval(input("enter a list of items: "))
+
+elementX = input("enter the item to search for: ")
+
+
+print("the index is", LinearSort(itemList, elementX))
