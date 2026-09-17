023751a891db78b18bf6a1fb381c04c279657e87
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-09-17 23:28:37 +0100

    Update data base programme.py

--- 6thYearLeavingCert/data base programme.py
+++ 6thYearLeavingCert/data base programme.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-DB_connection = sqlite3.connection("jobDataBaseFile.db")
+DB_connection = sqlite3.connect("jobDataBaseFile.db")
 
 cursor = DB_connection.cursor()
 
-jobDataBase = '''create table if not exists jobDataBase(
+CreateJobDataBase = '''create table if not exists jobDataBase(
 id integer primary key autoincrement,
 name text not null,
 department text not null,
@@ -12,15 +12,29 @@ salary integer
 ) STRICT'''
 
 
-cursor.execute(jobDataBase)
+cursor.execute(CreateJobDataBase)
 
 DB_connection.commit()
 
 #data to be put into database
-data = [["John Doe", "Engineeing", 75000],["Jane Smith", "HR", 60000],["Mike Johns", "Finance", 70000],["Sarah Brown", "Marketing", 65000],["Chris White", "Sales", 62000]]
+data = [["John_Doe", "Engineeing", 75000],["Jane_Smith", "HR", 60000],["Mike_Johns", "Finance", 70000],["Sarah_Brown", "Marketing", 65000],["Chris_White", "Sales", 62000]]
 
 
 for i in range(len(data)):
     name = data[i][0]
-    department
-    addData = '''insert into jobDataBase ()
+    department = data[i][1]
+    salary = data[i][2]
+    addData = '''insert into jobDataBase (name,department,salary) values (?,?,?)'''
+    cursor.execute(addData, (name,department,salary))
+    DB_connection.commit()
+
+
+fetchData = '''select name, department, salary from jobDataBase'''
+cursor.execute(fetchData)
+for row in cursor:
+    print(row)
+    
+    
+cursor.close()
+DB_connection.close()
+
