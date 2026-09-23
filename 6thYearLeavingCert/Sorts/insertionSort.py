fef2a64324920ddffc846b87281de9b5e545cf01
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-09-23 20:51:06 +0100

    Update insertionSort.py

--- 6thYearLeavingCert/Sorts/insertionSort.py
+++ 6thYearLeavingCert/Sorts/insertionSort.py
@@ -9,7 +9,8 @@ for i in range(1,len(unsortedList)):
         for i1 in range(i-1, -1, -1):
             if marker < unsortedList[i1]:
                 unsortedList[i1+1], unsortedList[i1] = unsortedList[i1], unsortedList[i1+1]
-                
+            else:
+                break
                 
                 
 print("the sorted list is: ", unsortedList)
