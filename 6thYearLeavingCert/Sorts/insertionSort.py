79c55c7e4b725c7c85de2876d84d50d68611045b
Luka-kvesic <22lkvesic.acc@lwetb.ie>
2026-09-23 10:28:11 +0100

    Add files via upload

---
+++ 6thYearLeavingCert/Sorts/insertionSort.py
@@ -0,0 +1,15 @@
+
+
+
+unsortedList = eval(input("enter a list: "))
+
+#[11,7,14,19,12]
+for i in range(1,len(unsortedList)):
+        marker = unsortedList[i]
+        for i1 in range(i-1, -1, -1):
+            if marker < unsortedList[i1]:
+                unsortedList[i1+1], unsortedList[i1] = unsortedList[i1], unsortedList[i1+1]
+                
+                
+                
+print("the sorted list is: ", unsortedList)
